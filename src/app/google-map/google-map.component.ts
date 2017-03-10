6b9a6c89703b754142ad28235a4ed9d84476dc46
Indyn <indynormal@gmail.com>
2017-03-10 16:04:05 +0700

    maps

--- src/app/google-map/google-map.component.ts
+++ src/app/google-map/google-map.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {LatLngLiteral} from "angular2-google-maps/core";
 
+import { MapsAPILoader } from 'angular2-google-maps/core';
+declare var google: any;
+
 @Component({
   selector: 'app-google-map',
   templateUrl: './google-map.component.html',
@@ -20,14 +23,16 @@ export class GoogleMapComponent implements OnInit {
   circles = [];
   paths:Array<LatLngLiteral> = [];
 
-  constructor() { }
+  area;
+  distance;
+
+  constructor(private mapsAPILoader: MapsAPILoader) { }
 
   ngOnInit() {
   }
 
   drawMap(event){
     if(this.choice != 0){
-
       if(this.choice == 1){
         this.marks.push({lat: event.coords.lat, lng: event.coords.lng});
       }else if(this.choice == 2){
@@ -36,6 +41,19 @@ export class GoogleMapComponent implements OnInit {
         this.points.push({lat: event.coords.lat, lng: event.coords.lng});
       }else if(this.choice == 4){
         this.pointsMap.push({lat: event.coords.lat, lng: event.coords.lng});
+
+        if(this.pointsMap.length >= 2){
+          this.mapsAPILoader.load().then(() => {
+            let locations = [];
+
+            for(let i=0;i<this.pointsMap.length;i++){
+              locations[i] = new google.maps.LatLng(this.pointsMap[i].lat, this.pointsMap[i].lng)
+            }
+            this.distance = google.maps.geometry.spherical.computeLength(locations);
+            console.log(this.distance);
+          });
+        }
+
       }
     }
   }
@@ -45,6 +63,16 @@ export class GoogleMapComponent implements OnInit {
     if(this.points.length > 0){
       if(event.latLng.lat() == this.points[0].lat && event.latLng.lng()  == this.points[0].lng){
         this.paths =  this.points;
+
+        this.mapsAPILoader.load().then(() => {
+          let locations = [];
+          for(let i=0;i<this.paths.length;i++){
+            locations[i] = new google.maps.LatLng(this.paths[i].lat, this.paths[i].lng)
+          }
+          this.area = google.maps.geometry.spherical.computeArea(locations);
+          console.log( this.area);
+        });
+
         this.points = [];
       }
     }
